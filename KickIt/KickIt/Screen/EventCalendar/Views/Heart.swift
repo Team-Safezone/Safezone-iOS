d64969cca0fa959c8a8c0b80a274f7f7942021ef
daeun127 <68541900+daeun127@users.noreply.github.com>
2024-05-10 14:51:42 +0900

    📦️ Build: 화면 추가

---
+++ KickIt/KickIt/Screen/EventCalendar/Views/Heart.swift
@@ -0,0 +1,18 @@
+//
+//  Heart.swift
+//  KickIt
+//
+//  Created by DaeunLee on 5/10/24.
+//
+
+import SwiftUI
+
+struct Heart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Heart()
+}
