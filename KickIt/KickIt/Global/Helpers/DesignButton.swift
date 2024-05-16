9938f4e7b2e7ef41d1ece1df795798b5f35cc3ff
daeun127 <68541900+daeun127@users.noreply.github.com>
2024-05-16 14:50:10 +0900

    🎉 Feat: 디자인 버튼 시스템 파일 추가

---
+++ KickIt/KickIt/Global/Helpers/DesignButton.swift
@@ -0,0 +1,18 @@
+//
+//  DesignButton.swift
+//  KickIt
+//
+//  Created by DaeunLee on 5/16/24.
+//
+
+import SwiftUI
+
+struct DesignButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DesignButton()
+}
