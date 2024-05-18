5c3612de8aad12596c3c17d27b557f1d2ba144f5
Yoonji Lee <yooonji08@naver.com>
2024-05-18 21:50:48 +0900

    🎉 Feat: date타입을 원하는 형식의 string타입으로 변경하는 함수 추가 #8

--- KickIt/KickIt/Global/Helpers/DateExtensions.swift
+++ KickIt/KickIt/Global/Helpers/DateExtensions.swift
@@ -32,3 +32,15 @@ func timeToString(time: Date) -> String {
 func isSameDay(date1: Date, date2: Date) -> Bool {
     return Calendar.current.isDate(date1, inSameDayAs: date2)
 }
+
+/// Date -> 0월 0일 (요일) 형식으로 변경하는 함수
+func dateToString2(date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MM월 dd일 (E)"
+    dateFormatter.locale = Locale.autoupdatingCurrent // 사용자의 위치에 따른 로케일
+    //dateFormatter.locale = Locale(identifier: "ko_KR") // 대한민국 로케일
+    
+    let dateToString = dateFormatter.string(from: date)
+    
+    return dateToString
+}
