0e1f85a2480c12f8f9323d5a1c6dfb3a6872060d
Yoonji Lee <yooonji08@naver.com>
2024-06-04 14:07:44 +0900

    ✅ Chore: 커스텀 폰트에 자간 파라미터 추가 #17

--- KickIt/KickIt/Global/Utils/TextStyle.swift
+++ KickIt/KickIt/Global/Utils/TextStyle.swift
@@ -7,25 +7,28 @@
 
 import SwiftUI
 
+/// 우리앱의 타이포그래피(커스텀 폰트 + 자간 + 행간)를 설정해둔 파일
 struct TextStyle {
     let font: Font
     let lineSpacing: CGFloat
+    let tracking: CGFloat
     
-    init(font: Font, lineSpacing: CGFloat) {
+    init(font: Font, lineSpacing: CGFloat, tracking: CGFloat) {
         self.font = font
         self.lineSpacing = lineSpacing
+        self.tracking = tracking
     }
     
-    static let H1Style = TextStyle(font: .H1, lineSpacing: 30)
-    static let H2Style = TextStyle(font: .H2, lineSpacing: 26)
-    static let Title1Style = TextStyle(font: .Title1, lineSpacing: 24)
-    static let Title2Style = TextStyle(font: .Title2, lineSpacing: 24)
-    static let SemiTitleStyle = TextStyle(font: .SemiTitle, lineSpacing: 20)
-    static let Body1Style = TextStyle(font: .Body1, lineSpacing: 24)
-    static let Body2Style = TextStyle(font: .Body2, lineSpacing: 20)
-    static let Body3Style = TextStyle(font: .Body3, lineSpacing: 20)
-    static let Caption1Style = TextStyle(font: .Caption1, lineSpacing: 18)
-    static let Caption2Style = TextStyle(font: .Caption2, lineSpacing: 18)
+    static let H1Style = TextStyle(font: .H1, lineSpacing: 30, tracking: 0)
+    static let H2Style = TextStyle(font: .H2, lineSpacing: 26, tracking: 0)
+    static let Title1Style = TextStyle(font: .Title1, lineSpacing: 24, tracking: -4)
+    static let Title2Style = TextStyle(font: .Title2, lineSpacing: 24, tracking: -4)
+    static let SemiTitleStyle = TextStyle(font: .SemiTitle, lineSpacing: 20, tracking: 0)
+    static let Body1Style = TextStyle(font: .Body1, lineSpacing: 24, tracking: 0)
+    static let Body2Style = TextStyle(font: .Body2, lineSpacing: 20, tracking: 0)
+    static let Body3Style = TextStyle(font: .Body3, lineSpacing: 20, tracking: 0)
+    static let Caption1Style = TextStyle(font: .Caption1, lineSpacing: 18, tracking: 0)
+    static let Caption2Style = TextStyle(font: .Caption2, lineSpacing: 18, tracking: 0)
 }
 
 struct PretendardTextModifier: ViewModifier {
@@ -35,11 +38,12 @@ struct PretendardTextModifier: ViewModifier {
         content
             .font(textStyle.font)
             .lineSpacing(textStyle.lineSpacing)
+            .tracking(textStyle.tracking)
     }
 }
 
 extension View {
-    /// line height + pretnedard font 확장 함수
+    /// letter spacing + line height + pretnedard font 확장 함수
     func pretendardTextStyle(_ style: TextStyle) -> some View {
         self.modifier(PretendardTextModifier(textStyle: style))
     }
