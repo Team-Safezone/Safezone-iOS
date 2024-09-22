ca24b1ba1ed76a232111d36140b273886d5654d2
Yoonji Lee <yooonji08@naver.com>
2024-09-22 17:52:05 +0900

    🔧 Fix: 경기캘린더 화면 내 API 모두 이전 API응답 모델로 변경 #41
    - gradient 오류 수정
    - 응답 모델 변경(추후 백엔드가 새로운 응답 모델로 변경시, 다시 원상복구 필요)

--- KickIt/KickIt/Network/Models/Response/SoccerMatchMonthlyResponse.swift
+++ KickIt/KickIt/Network/Models/Response/SoccerMatchMonthlyResponse.swift
@@ -9,13 +9,14 @@ import Foundation
 
 /// 한달 경기 일정 조회 Response 모델
 struct SoccerMatchMonthlyResponse: Codable {
-    let soccerSeason: String // 축구 시즌
-    let matchDates: Array<String> // 축구 경기 날짜
-    let soccerTeamNames: Array<String> // 팀 이름
+    let matchDates: String // 축구 경기 날짜
+    //let soccerSeason: String // 축구 시즌
+    //let matchDates: [String] // 축구 경기 날짜
+    //let soccerTeamNames: Array<String> // 팀 이름
     
     enum CodingKeys: String, CodingKey {
-        case soccerSeason = "soccerSeason"
+        //case soccerSeason = "soccerSeason"
         case matchDates = "date"
-        case soccerTeamNames = "teamNames"
+        //case soccerTeamNames = "teamNames"
     }
 }
